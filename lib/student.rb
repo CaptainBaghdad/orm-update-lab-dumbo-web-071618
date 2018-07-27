d76398fc9584e7a007deb79e0572ed5376284f14
Jason Pitts <juniorjr083@gmail.com>
2018-07-27 02:27:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,71 @@
 require_relative "../config/environment.rb"
 
 class Student
-
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade 
+  attr_reader :id
+
+  def initialize(name, grade, id =nil)
+    #binding.pry
+   @name = name 
+   @grade = grade 
+   @id = id
+
+  end 
+
+
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY, 
+    name TEXT,
+    grade TEXT
+   )
+      SQL
+
+  DB[:conn].execute(sql)
+
+  end 
+
+  def self.drop_table
+    sql = "DROP TABLE students"
+    DB[:conn].execute(sql)
+
+end
+
+def save
+  if self.id
+      self.update
+    else
+  sql = "INSERT INTO students(name,grade) VALUES(?,?)"
+  DB[:conn].execute(sql, self.name,self.grade)
+  #@id = DB[:conn].execute('SELECT last_insert_rowid() FROM students')[0][0]
+last  = DB[:conn].execute("SELECT * FROM students")
+@id = last.flatten[0]
+end
+end
+
+def self.create(name,grade)
+  student = Student.new(name,grade)
+  student.save 
+  student
+  
+end 
+
+def update
+    sql = <<-SQL
+      UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+
+def self.new_from_db
+  
+  
+end
 
 end
